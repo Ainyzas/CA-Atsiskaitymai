61391ba020a830c778933a823f49d2a22425d0fc
ainas <puidokasainas@gmail.com>
2023-01-30 20:17:26 +0200

    completed task, brand data is displayed correctly

--- atsiskaitymas2/JS Tasks/Task 4/script.js
+++ atsiskaitymas2/JS Tasks/Task 4/script.js
@@ -8,4 +8,33 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+const output = document.querySelector("#output");
+
+async function displayBrands() {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+
+    data.forEach(brand => {
+        const brandCard = document.createElement("div");
+        brandCard.classList.add("brand");
+
+        const brandCardName = document.createElement("h2");
+        brandCardName.classList.add("brandName");
+        brandCardName.textContent = brand.brand;
+        brandCard.append(brandCardName);
+
+        const models = brand.models;
+        models.forEach(model => {
+            const brandCardModel = document.createElement("h4");
+            brandCardModel.classList.add("brandItems");
+            brandCardModel.textContent = model;
+            brandCard.append(brandCardModel);
+        });
+
+        output.append(brandCard);
+    });
+}
+
+displayBrands();
