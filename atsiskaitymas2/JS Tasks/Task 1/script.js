1da4ccc389aeddb70a1ef38edf5a3a5840fff058
ainas <puidokasainas@gmail.com>
2023-01-30 18:50:05 +0200

    finished task 1 with successful kg conversion

--- atsiskaitymas2/JS Tasks/Task 1/script.js
+++ atsiskaitymas2/JS Tasks/Task 1/script.js
@@ -8,3 +8,27 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+const input = document.querySelector("#search");
+const output = document.querySelector("#output");
+
+function convert(e) {
+    e.preventDefault();
+    output.innerHTML = "";
+    const kg = input.value;
+
+    const lb = kg * 2.2046;
+    const lbOutput = document.createElement("div");
+    lbOutput.textContent = lb + " lb";
+
+    const g = kg / 0.001;
+    const gOutput = document.createElement("div");
+    gOutput.textContent = g + " g";
+
+    const oz = kg * 35.274;
+    const ozOutput = document.createElement("div");
+    ozOutput.textContent = oz + " oz";
+
+    output.append(lbOutput, gOutput, ozOutput);
+}
+
+document.querySelector("form").addEventListener("submit", convert);
