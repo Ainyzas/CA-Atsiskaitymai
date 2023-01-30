90023b32065dd2bcbdbdc7598ee153f4398acf6f
ainas <puidokasainas@gmail.com>
2023-01-30 20:34:00 +0200

    completed given filtering tasks

--- atsiskaitymas2/JS Tasks/Task 5/script.js
+++ atsiskaitymas2/JS Tasks/Task 5/script.js
@@ -9,13 +9,27 @@ atveju grąžins masyvą su "users", kurie yra pilnamečiai.
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20, hasDog: true },
-  { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
-  { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
-  { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
-  { id: '5', name: 'Alex John', age: 25, hasDog: true },
-  { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
-  { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
-  { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
-  { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
+    { id: "1", name: "John Smith", age: 20, hasDog: true },
+    { id: "2", name: "Ann Smith", age: 24, hasDog: false },
+    { id: "3", name: "Tom Jones", age: 31, hasDog: true },
+    { id: "4", name: "Rose Peterson", age: 17, hasDog: false },
+    { id: "5", name: "Alex John", age: 25, hasDog: true },
+    { id: "6", name: "Ronald Jones", age: 63, hasDog: true },
+    { id: "7", name: "Elton Smith", age: 16, hasDog: true },
+    { id: "8", name: "Simon Peterson", age: 30, hasDog: false },
+    { id: "9", name: "Daniel Cane", age: 51, hasDog: true },
 ];
+
+function filterDogOwners(users) {
+    const dogOwners = users.filter(user => user.hasDog);
+    return dogOwners;
+}
+
+console.log(filterDogOwners(users));
+
+function filterAdults(users) {
+    const adults = users.filter(user => user.age > 18);
+    return adults;
+}
+
+console.log(filterAdults(users));
