392ab8b41393b1e339908fdb7acf43a5186b0164
ainas <puidokasainas@gmail.com>
2023-01-30 20:53:47 +0200

    completed task, object keys are displayed

--- atsiskaitymas2/JS Tasks/Task 7/script.js
+++ atsiskaitymas2/JS Tasks/Task 7/script.js
@@ -5,8 +5,15 @@ Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-  make: 'audi',
-  model: 'A6',
-  year: 2005,
-  color: 'white',
+    make: "audi",
+    model: "A6",
+    year: 2005,
+    color: "white",
 };
+
+function showObjectKeys(object) {
+    const keys = Object.keys(object);
+    return keys;
+}
+
+console.log(showObjectKeys(audi));
