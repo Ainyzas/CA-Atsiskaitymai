d54c88dc9b4cba2e455ac5ffaaafb9e44f5879f7
ainas <puidokasainas@gmail.com>
2023-01-31 18:49:43 +0200

    fixed movie name typo

--- atsiskaitymas2/JS Tasks/Task 9/script.js
+++ atsiskaitymas2/JS Tasks/Task 9/script.js
@@ -25,6 +25,6 @@ const movie1 = new Movie("Spider-Man 3", "Sam Raimi", 258_000_000);
 console.log(movie1);
 console.log(movie1.wasExpensive());
 
-const movie2 = new Movie("The Blair With Project", "Daniel Myrick", 600_000);
+const movie2 = new Movie("The Blair Witch Project", "Daniel Myrick", 600_000);
 console.log(movie2);
 console.log(movie2.wasExpensive());
